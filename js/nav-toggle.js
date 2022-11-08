39264db5d15f94c5cb17479f1a3d715edd5ac767
Alan Jones <alan@hootreading.com>
2022-11-07 19:00:40 -0500

    feature/responsive-nav - created responsive nav menu, refactored css to modular files, slight tweaks for responsive

---
+++ js/nav-toggle.js
@@ -0,0 +1,11 @@
+// Separating this javascript from <script> code on home to separate js file.
+
+/* Only for demo purposes as this script will not be used for the nav 
+but future scripts will go into this js directory. */
+
+// const toggleButton = document.getElementById("toggle-button");
+// const naviList = document.getElementById("navi-list");
+
+// toggleButton.addEventListener("click", () => {
+//   naviList.classList.toggle("active");
+// });
